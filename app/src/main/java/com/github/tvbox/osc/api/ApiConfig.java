e4a2c8b4b2cde50850e9e58f1e2d6f674d7489ff
pingzj <161149721+pingzj@users.noreply.github.com>
2024-02-25 01:16:25 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -128,7 +128,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://我不是.摸鱼儿.top");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
